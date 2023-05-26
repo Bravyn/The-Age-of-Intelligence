ab3e198550a094053d3ca4486cef4644fe70702c
Bravyn <ianbravynsa@gmail.com>
2023-05-26 19:49:30 +0000

    made the submit button functional

--- script.js
+++ script.js
@@ -6,4 +6,25 @@ const submitButton = document.getElementById('submit-button');
 // Event listener for submit button click
 submitButton.addEventListener('click', processInput);
 
-//
+//event listener for enter key press on the input field
+inputField.addEventListener('click', function(event) {
+  if (event.key === 'Enter') {
+    processInput();
+  }
+});
+
+//this is where we process input affter submission
+function processInput(){
+  const input = inputField.value.trim();//remove whitespace
+  inputField.value = '';//reset input text field
+  //let's just append input to output text for now
+  appendToOutput('> ' + input);
+}
+
+const appendToOutput = ((text) => {
+  outputArea.innerHTML += text + '<br/>';
+  outputArea.scrollTop = outputArea.scrollHeight//autoscrolls to the bottom
+})
+  
+//Display a welcome message when the game starts
+appendToOutput('Welcome to The Age of Intelligence! Enter commands to play the game.');
