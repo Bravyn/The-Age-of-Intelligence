9e7d53ef69b0f027cc4516e3818992d55c9a0c0a
Bravyn <ianbravynsa@gmail.com>
2023-05-26 19:27:27 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Get the necessary HTML elements
+const outputArea = document.getElementById('output-area');
+const inputField = document.getElementById('input-field');
+const submitButton = document.getElementById('submit-button');
+
+// Event listener for submit button click
+submitButton.addEventListener('click', processInput);
+
+//
